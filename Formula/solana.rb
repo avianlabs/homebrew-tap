fc6aa28efa52c10710e42c361be68c7181bc0588
Dominic Black <me@jdm.black>
2024-07-24 15:00:39 +0000

    solana: update 1.18.18 bottle.

--- Formula/solana.rb
+++ Formula/solana.rb
@@ -26,13 +26,10 @@ class Solana < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0a4925bf73cd599551265f2bd34ab77ff85f06d7e04ae06d8b6f6ddd9b62e744"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "67b90b24bb58f85a1bd9ed9d7e355c58d345327d1d9fa4cd5230a124dcd227ea"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "40618558a1d2ef9161bd2a4e191df55682742fda7fa264450c47374fc2dab2b8"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d91a92fa51db05ceaf8a33a4040cdd8baf74f0c03750f008e4d3e30e71d8431d"
-    sha256 cellar: :any_skip_relocation, ventura:        "fa00e798ee22cd33f88669b50f2f33cb8f3ea2176567ea8c01a09e3192fa9e57"
-    sha256 cellar: :any_skip_relocation, monterey:       "7873dc2bc52b3d0a434ec9001eaed2a9547354e7d5b29c711f4a8f63024a5fa5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e75b09886e90f8c29ed7ee100975715401f0ccfacd52d6199f97ee9ad3893bf3"
+    root_url "https://ghcr.io/v2/avianlabs/tap"
+    sha256 cellar: :any,                 arm64_sonoma: "60de43cdfc66ff333a1c3d8792d1a91d2afb74bff1ab7b288e8340885f20470e"
+    sha256 cellar: :any,                 ventura:      "a7db839ff13746ad1295451f73708c6e2c79ed9a10c123f7d03ac6e8baf9d4b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "53b2afd7b5fbc000ea34c8239f9e231587ccd5eb889becf6f5ad1b7801552fef"
   end
 
   depends_on "protobuf" => :build
